4daff4ac715e8743eda0ec89148c1061b889bab6
igara <igara1119@gmail.com>
2021-12-19 01:42:06 +0900

    modify: default file. add script test

---
+++ src/scripts/__tests__/templates/default.test.ts
@@ -0,0 +1,261 @@
+import fs from 'fs';
+
+const spyConsoleError = jest.spyOn(global.console, 'error').mockImplementation();
+const spyConsoleInfo = jest.spyOn(global.console, 'info').mockImplementation();
+const spyFSWriteFileSync = jest.spyOn(fs, 'writeFileSync').mockImplementation();
+const spyFSMkdirSync = jest.spyOn(fs, 'mkdirSync').mockImplementation();
+const processArgv = process.argv;
+
+describe('template script', () => {
+  beforeEach(() => {
+    jest.resetModules();
+    spyConsoleError.mockReset();
+    spyConsoleInfo.mockReset();
+    spyFSWriteFileSync.mockReset();
+    spyFSMkdirSync.mockReset();
+    process.argv = processArgv;
+  });
+
+  test('yarn templateのパス指定がないとき', async () => {
+    const templates = (await import('../../templates')) as any;
+
+    const status = templates.default.main();
+    expect([
+      [
+        `\u001b[31m
+    not found 'paths=xxxxx'
+
+    example)
+    yarn templates path=projects/syonet_eight_design_system/src/components/xxxx
+    or
+    yarn templates path=projects/syonet_eight_www/components/xxxx
+    `,
+      ],
+    ]).toEqual(spyConsoleError.mock.calls);
+    expect([]).toEqual(spyConsoleInfo.mock.calls);
+
+    expect(1).toBe(status);
+  });
+
+  test('yarn templateのパス指定が適切ではないとき', async () => {
+    process.argv = [
+      'node',
+      'projects/syonet_eight_design_system/src/scripts/templates.ts',
+      'path=projects/syonet_eight_yyyy/components/xxxx/yyyy',
+    ];
+
+    const templates = (await import('../../templates')) as any;
+
+    const status = templates.default.main();
+    expect([
+      [
+        `\u001b[31m
+    not found projects 'paths=projects/syonet_eight_xxxxx'
+
+    example)
+    yarn templates path=projects/syonet_eight_design_system/src/components/xxxx
+    or
+    yarn templates path=projects/syonet_eight_www/components/xxxx
+    `,
+      ],
+    ]).toEqual(spyConsoleError.mock.calls);
+    expect([]).toEqual(spyConsoleInfo.mock.calls);
+    expect(1).toBe(status);
+  });
+
+  test('yarn templateのパス指定が適切であるとき（design_system）', async () => {
+    process.argv = [
+      'node',
+      'projects/syonet_eight_design_system/src/scripts/templates.ts',
+      'path=projects/syonet_eight_design_system/src/components/hoge/fuga',
+    ];
+
+    const templates = (await import('../../templates')) as any;
+
+    const status = templates.default.main();
+    expect([]).toEqual(spyConsoleError.mock.calls);
+    expect(2).toBe(spyConsoleInfo.mock.calls.length);
+    expect(3).toBe(spyFSMkdirSync.mock.calls.length);
+    expect(6).toBe(spyFSWriteFileSync.mock.calls.length);
+    expect(`import React from 'react';
+import TestingLibrary from '@testing-library/react';
+import { Fuga } from '../../';
+
+describe('Fuga', () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
+  test('初期表示', () => {
+    const renderTest = TestingLibrary.render(<Fuga />);
+    expect('<div></div>').toBe(renderTest.baseElement.innerHTML);
+  });
+});
+`).toBe(spyFSWriteFileSync.mock.calls[0][1]);
+    expect(`export * from './fuga';
+`).toBe(spyFSWriteFileSync.mock.calls[1][1]);
+    expect(`import { Meta, Story, Source, Props } from '@storybook/addon-docs';
+import { Fuga } from '../';
+
+<Meta title="design_system/hoge/fuga/default" component={Fuga} />
+
+# design_system/hoge/fuga/default
+
+fuga の説明
+
+<Story
+  name="default"
+  args={{
+    size: 20,
+    color: 'black',
+  }}></Story>
+
+<Source>
+  <Fuga />
+</Source>
+
+<Props story="default" />
+`).toBe(spyFSWriteFileSync.mock.calls[2][1]);
+    expect(`import * as CSS from 'csstype';
+import { css } from '@emotion/react';
+
+export interface Props {
+  size: number;
+  color: CSS.Property.Color;
+}
+
+export const styles = {
+  fuga: (props: Props) => css\`
+    width: \${props.size}px;
+    height: \${props.size}px;
+    color: \${props.color};
+  \`,
+};
+`).toBe(spyFSWriteFileSync.mock.calls[3][1]);
+    expect(`import React from 'react';
+import * as Styels from './fuga.styles';
+
+interface Props {
+  size?: Styels.Props['size'];
+  color?: Styels.Props['color'];
+}
+
+export const Fuga: React.FC<Props> = (props) => {
+  return <></>;
+};
+`).toBe(spyFSWriteFileSync.mock.calls[4][1]);
+    expect(`import React from 'react';
+
+export const useXXXState = (flag?: boolean) => {
+  return React.useState(flag);
+};
+
+export const useXXXCallback = () => {
+  return React.useCallback(() => {}, []);
+};
+
+export const useXXXEffect = (args: {}) => {
+  return React.useEffect(() => {}, [args]);
+};
+`).toBe(spyFSWriteFileSync.mock.calls[5][1]);
+    expect(0).toBe(status);
+  });
+
+  test('yarn templateのパス指定が適切であるとき（www）', async () => {
+    process.argv = [
+      'node',
+      'projects/syonet_eight_design_system/src/scripts/templates.ts',
+      'path=projects/syonet_eight_www/components/hoge/fuga',
+    ];
+
+    const templates = (await import('../../templates')) as any;
+
+    const status = templates.default.main();
+    expect([]).toEqual(spyConsoleError.mock.calls);
+    expect(2).toBe(spyConsoleInfo.mock.calls.length);
+    expect(3).toBe(spyFSMkdirSync.mock.calls.length);
+    expect(6).toBe(spyFSWriteFileSync.mock.calls.length);
+    expect(`import React from 'react';
+import TestingLibrary from '@testing-library/react';
+import { Fuga } from '../../';
+
+describe('Fuga', () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
+  test('初期表示', () => {
+    const renderTest = TestingLibrary.render(<Fuga />);
+    expect('<div></div>').toBe(renderTest.baseElement.innerHTML);
+  });
+});
+`).toBe(spyFSWriteFileSync.mock.calls[0][1]);
+    expect(`export * from './fuga';
+`).toBe(spyFSWriteFileSync.mock.calls[1][1]);
+    expect(`import { Meta, Story, Source, Props } from '@storybook/addon-docs';
+import { Fuga } from '../';
+
+<Meta title="www/hoge/fuga/default" component={Fuga} />
+
+# www/hoge/fuga/default
+
+fuga の説明
+
+<Story
+  name="default"
+  args={{
+    size: 20,
+    color: 'black',
+  }}></Story>
+
+<Source>
+  <Fuga />
+</Source>
+
+<Props story="default" />
+`).toBe(spyFSWriteFileSync.mock.calls[2][1]);
+    expect(`import * as CSS from 'csstype';
+import { css } from '@emotion/react';
+
+export interface Props {
+  size: number;
+  color: CSS.Property.Color;
+}
+
+export const styles = {
+  fuga: (props: Props) => css\`
+    width: \${props.size}px;
+    height: \${props.size}px;
+    color: \${props.color};
+  \`,
+};
+`).toBe(spyFSWriteFileSync.mock.calls[3][1]);
+    expect(`import React from 'react';
+import * as Styels from './fuga.styles';
+
+interface Props {
+  size?: Styels.Props['size'];
+  color?: Styels.Props['color'];
+}
+
+export const Fuga: React.FC<Props> = (props) => {
+  return <></>;
+};
+`).toBe(spyFSWriteFileSync.mock.calls[4][1]);
+    expect(`import React from 'react';
+
+export const useXXXState = (flag?: boolean) => {
+  return React.useState(flag);
+};
+
+export const useXXXCallback = () => {
+  return React.useCallback(() => {}, []);
+};
+
+export const useXXXEffect = (args: {}) => {
+  return React.useEffect(() => {}, [args]);
+};
+`).toBe(spyFSWriteFileSync.mock.calls[5][1]);
+    expect(0).toBe(status);
+  });
+});
--- src/scripts/templates.ts
+++ src/scripts/templates.ts
@@ -1,146 +1,170 @@
 import path from 'path';
 import fs from 'fs';
 
-const colors = {
-  red: '\u001b[31m',
-  green: '\u001b[32m',
-};
+export const main = () => {
+  const colors = {
+    red: '\u001b[31m',
+    green: '\u001b[32m',
+  };
 
-const componentPathKeyValue = process.argv.join().match(/path=\S*/);
-if (!(componentPathKeyValue && componentPathKeyValue.length)) {
-  console.error(`${colors.red}
-not found 'paths=xxxxx'
+  const componentPathKeyValue = process.argv.join().match(/path=\S*/);
+  if (!(componentPathKeyValue && componentPathKeyValue.length)) {
+    console.error(`${colors.red}
+    not found 'paths=xxxxx'
 
-example)
-yarn templates path=projects/syonet_eight_design_system/src/components/xxxx
-or
-yarn templates path=projects/syonet_eight_www/components/xxxx
-`);
-  process.exit(-1);
-}
+    example)
+    yarn templates path=projects/syonet_eight_design_system/src/components/xxxx
+    or
+    yarn templates path=projects/syonet_eight_www/components/xxxx
+    `);
+    return 1;
+  }
 
-const componentPath = componentPathKeyValue[0].replace('path=', '');
+  const componentPath = componentPathKeyValue[0].replace('path=', '');
 
-const projects = {
-  syonet_eight_design_system: {
-    paths: {
-      components: 'projects/syonet_eight_design_system/src/components',
+  const projects = {
+    syonet_eight_design_system: {
+      paths: {
+        components: 'projects/syonet_eight_design_system/src/components',
+      },
+      title: 'design_system',
     },
-    title: 'design_system',
-  },
-  syonet_eight_www: {
-    paths: {
-      components: 'projects/syonet_eight_www/components',
+    syonet_eight_www: {
+      paths: {
+        components: 'projects/syonet_eight_www/components',
+      },
+      title: 'www',
     },
-    title: 'www',
-  },
-};
+  };
 
-const projectNames = Object.keys(projects);
-const foundProjectName = projectNames.find((projectName) => {
-  const projectComponentPath = projects[projectName].paths.components;
-  return new RegExp(projectComponentPath).test(componentPath);
-});
-
-if (!foundProjectName) {
-  console.error(`${colors.red}
-not found projects 'paths=xxxxx'
-
-example)
-yarn templates path=projects/syonet_eight_design_system/src/components/xxxx
-or
-yarn templates path=projects/syonet_eight_www/components/xxxx
-`);
-  process.exit(-1);
-}
-
-const camelCase = (str: string) => {
-  const s = str.charAt(0).toLowerCase() + str.slice(1);
-  return s.replace(/[-_](.)/g, (_, group1) => {
-    return group1.toUpperCase();
+  const projectNames = Object.keys(projects);
+  const foundProjectName = projectNames.find((projectName) => {
+    const projectComponentPath = projects[projectName].paths.components;
+    return new RegExp(projectComponentPath).test(componentPath);
   });
+
+  if (!foundProjectName) {
+    console.error(`${colors.red}
+    not found projects 'paths=projects/syonet_eight_xxxxx'
+
+    example)
+    yarn templates path=projects/syonet_eight_design_system/src/components/xxxx
+    or
+    yarn templates path=projects/syonet_eight_www/components/xxxx
+    `);
+    return 1;
+  }
+
+  const camelCase = (str: string) => {
+    const s = str.charAt(0).toLowerCase() + str.slice(1);
+    return s.replace(/[-_](.)/g, (_, group1) => {
+      return group1.toUpperCase();
+    });
+  };
+
+  const pascalCase = (str: string) => {
+    var camel = camelCase(str);
+    return camel.charAt(0).toUpperCase() + camel.slice(1);
+  };
+
+  const dirNames = componentPath.split('/');
+  const filename = dirNames[dirNames.length - 1];
+  const componentNames = {
+    filename,
+    varname: pascalCase(filename),
+  };
+
+  const componentFullPath = path.resolve(__dirname, `../../../../${componentPath}`);
+
+  fs.mkdirSync(componentFullPath, {
+    recursive: true,
+  });
+
+  console.info(`${colors.green}
+  mkdir ${componentFullPath}`);
+
+  const templates = {
+    'index.ts': fs
+      .readFileSync(path.resolve(__dirname, './templates/index.ts'))
+      .toString()
+      .replace(/xxxxx/g, componentNames.filename)
+      .replace(/XXXXX/g, componentNames.varname),
+    'stories/default.stories.mdx': fs
+      .readFileSync(path.resolve(__dirname, './templates/stories/default.stories.mdx'))
+      .toString()
+      .replace(/xxxxx/g, componentNames.filename)
+      .replace(/XXXXX/g, componentNames.varname)
+      .replace(
+        /TITLE/g,
+        `${projects[foundProjectName].title}${componentPath.replace(
+          projects[foundProjectName].paths.components,
+          '',
+        )}`,
+      ),
+    'xxxxx.styles.ts': fs
+      .readFileSync(path.resolve(__dirname, './templates/xxxxx.styles.ts'))
+      .toString()
+      .replace(/xxxxx/g, componentNames.filename)
+      .replace(/XXXXX/g, componentNames.varname),
+    'xxxxx.tsx': fs
+      .readFileSync(path.resolve(__dirname, './templates/xxxxx.tsx'))
+      .toString()
+      .replace(/xxxxx/g, componentNames.filename)
+      .replace(/XXXXX/g, componentNames.varname),
+    'xxxxx.hooks.ts': fs
+      .readFileSync(path.resolve(__dirname, './templates/xxxxx.hooks.ts'))
+      .toString(),
+    '__tests__/xxxxx/default.test.tsx': fs
+      .readFileSync(
+        path.resolve(__dirname, './templates/__tests__/xxxxx/default.test.tsx'),
+      )
+      .toString()
+      .replace(/xxxxx/g, componentNames.filename)
+      .replace(/XXXXX/g, componentNames.varname),
+  };
+
+  fs.mkdirSync(`${componentFullPath}/__tests__/${componentNames.filename}`, {
+    recursive: true,
+  });
+  fs.writeFileSync(
+    `${componentFullPath}/__tests__/${componentNames.filename}/default.test.tsx`,
+    templates['__tests__/xxxxx/default.test.tsx'],
+  );
+  fs.writeFileSync(`${componentFullPath}/index.ts`, templates['index.ts']);
+  fs.mkdirSync(`${componentFullPath}/stories`, {
+    recursive: true,
+  });
+  fs.writeFileSync(
+    `${componentFullPath}/stories/dafault.stories.mdx`,
+    templates['stories/default.stories.mdx'],
+  );
+  fs.writeFileSync(
+    `${componentFullPath}/${componentNames.filename}.styles.ts`,
+    templates['xxxxx.styles.ts'],
+  );
+  fs.writeFileSync(
+    `${componentFullPath}/${componentNames.filename}.tsx`,
+    templates['xxxxx.tsx'],
+  );
+  fs.writeFileSync(
+    `${componentFullPath}/${componentNames.filename}.hooks.ts`,
+    templates['xxxxx.hooks.ts'],
+  );
+
+  console.info(`${colors.green}
+  writeFile ${componentFullPath}/__tests__/${componentNames.filename}/default.test.tsx
+  writeFile ${componentFullPath}/index.ts
+  writeFile ${componentFullPath}/${componentNames.filename}.stories.mdx
+  writeFile ${componentFullPath}/stories/default.styles.ts
+  writeFile ${componentFullPath}/${componentNames.filename}.tsx
+  writeFile ${componentFullPath}/${componentNames.filename}.hooks.ts
+  `);
+
+  return 0;
 };
 
-const pascalCase = (str: string) => {
-  var camel = camelCase(str);
-  return camel.charAt(0).toUpperCase() + camel.slice(1);
-};
-
-const dirNames = componentPath.split('/');
-const filename = dirNames[dirNames.length - 1];
-const componentNames = {
-  filename,
-  varname: pascalCase(filename),
-};
-
-const componentFullPath = path.resolve(__dirname, `../../../../${componentPath}`);
-
-fs.mkdirSync(componentFullPath, {
-  recursive: true,
-});
-
-console.info(`${colors.green}
-mkdir ${componentFullPath}`);
-
-const templates = {
-  'index.ts': fs
-    .readFileSync(path.resolve(__dirname, './templates/index.ts'))
-    .toString()
-    .replace(/xxxxx/g, componentNames.filename)
-    .replace(/XXXXX/g, componentNames.varname),
-  'xxxxx.stories.mdx': fs
-    .readFileSync(path.resolve(__dirname, './templates/xxxxx.stories.mdx'))
-    .toString()
-    .replace(/xxxxx/g, componentNames.filename)
-    .replace(/XXXXX/g, componentNames.varname)
-    .replace(
-      /TITLE/g,
-      `${projects[foundProjectName].title}${componentPath
-        .replace(projects[foundProjectName].paths.components, '')
-        .replace(new RegExp(`/${componentNames.filename}`), '')}`,
-    ),
-  'xxxxx.styles.ts': fs
-    .readFileSync(path.resolve(__dirname, './templates/xxxxx.styles.ts'))
-    .toString()
-    .replace(/xxxxx/g, componentNames.filename)
-    .replace(/XXXXX/g, componentNames.varname),
-  'xxxxx.tsx': fs
-    .readFileSync(path.resolve(__dirname, './templates/xxxxx.tsx'))
-    .toString()
-    .replace(/xxxxx/g, componentNames.filename)
-    .replace(/XXXXX/g, componentNames.varname),
-  '__tests__/xxxxx/xxxxx.test.tsx': fs
-    .readFileSync(path.resolve(__dirname, './templates/__tests__/xxxxx/xxxxx.test.tsx'))
-    .toString()
-    .replace(/xxxxx/g, componentNames.filename)
-    .replace(/XXXXX/g, componentNames.varname),
-};
-
-fs.mkdirSync(`${componentFullPath}/__tests__/${componentNames.filename}`, {
-  recursive: true,
-});
-fs.writeFileSync(
-  `${componentFullPath}/__tests__/${componentNames.filename}/${componentNames.filename}.test.tsx`,
-  templates['__tests__/xxxxx/xxxxx.test.tsx'],
-);
-fs.writeFileSync(`${componentFullPath}/index.ts`, templates['index.ts']);
-fs.writeFileSync(
-  `${componentFullPath}/${componentNames.filename}.stories.mdx`,
-  templates['xxxxx.stories.mdx'],
-);
-fs.writeFileSync(
-  `${componentFullPath}/${componentNames.filename}.styles.ts`,
-  templates['xxxxx.styles.ts'],
-);
-fs.writeFileSync(
-  `${componentFullPath}/${componentNames.filename}.tsx`,
-  templates['xxxxx.tsx'],
-);
-
-console.info(`${colors.green}
-writeFile ${componentFullPath}/__tests__/${componentNames.filename}/${componentNames.filename}.test.tsx
-writeFile ${componentFullPath}/index.ts
-writeFile ${componentFullPath}/${componentNames.filename}.stories.mdx
-writeFile ${componentFullPath}/${componentNames.filename}.styles.ts
-writeFile ${componentFullPath}/${componentNames.filename}.tsx
-`);
+if (process.execArgv.includes('esbuild-register')) {
+  // yarnによるnpm script起動想定
+  const status = main();
+  process.exit(status);
+}

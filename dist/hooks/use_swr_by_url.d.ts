a0287d04c926719bae069bb6043fbf90c961a448
igara <igara1119@gmail.com>
2021-12-27 00:14:15 +0900

    add: use_swr_by_url response type arg

--- dist/hooks/use_swr_by_url.d.ts
+++ dist/hooks/use_swr_by_url.d.ts
@@ -1,1 +1,3 @@
-export declare function useSWRByURL<Data = any, Error = any>(url: string): import("swr").SWRResponse<Data, Error>;
+declare type ResponseType = 'text' | 'json' | undefined;
+export declare function useSWRByURL<Data = any, Error = any>(url: string, type?: ResponseType): import("swr").SWRResponse<Data, Error>;
+export {};

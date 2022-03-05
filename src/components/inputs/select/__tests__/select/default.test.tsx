dfd7840e02e7b3cd6a9824410e6e2b380cb8a84c
igara <igara1119@gmail.com>
2022-03-05 17:59:55 +0900

    fix: test

--- src/components/inputs/select/__tests__/select/default.test.tsx
+++ src/components/inputs/select/__tests__/select/default.test.tsx
@@ -8,7 +8,18 @@ describe('Select', () => {
   });
 
   test('初期表示', () => {
-    const renderTest = TestingLibrary.render(<Select />);
-    expect('<div></div>').toBe(renderTest.baseElement.innerHTML);
+    const renderTest = TestingLibrary.render(
+      <Select
+        items={[
+          {
+            value: 'hoge',
+            label: 'hoge',
+          },
+        ]}
+      />,
+    );
+    expect(
+      '<div><div css="You have tried to stringify object returned from `css` function. It isn\'t supposed to be used directly (e.g. as value of the `className` prop), but rather handed to emotion so it can handle it (e.g. as value of `css` prop)."><select css="You have tried to stringify object returned from `css` function. It isn\'t supposed to be used directly (e.g. as value of the `className` prop), but rather handed to emotion so it can handle it (e.g. as value of `css` prop)."><option value="hoge">hoge</option></select><div css="You have tried to stringify object returned from `css` function. It isn\'t supposed to be used directly (e.g. as value of the `className` prop), but rather handed to emotion so it can handle it (e.g. as value of `css` prop)."></div><div css="You have tried to stringify object returned from `css` function. It isn\'t supposed to be used directly (e.g. as value of the `className` prop), but rather handed to emotion so it can handle it (e.g. as value of `css` prop)."><label>hoge</label></div><div css="You have tried to stringify object returned from `css` function. It isn\'t supposed to be used directly (e.g. as value of the `className` prop), but rather handed to emotion so it can handle it (e.g. as value of `css` prop)."><div css="You have tried to stringify object returned from `css` function. It isn\'t supposed to be used directly (e.g. as value of the `className` prop), but rather handed to emotion so it can handle it (e.g. as value of `css` prop)." data-value="hoge">hoge</div></div></div></div>',
+    ).toBe(renderTest.baseElement.innerHTML);
   });
 });

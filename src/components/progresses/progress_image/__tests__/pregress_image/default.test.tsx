ac921e9703fa5e2f7f5268cc07b10bbb21c8376f
igara <igara1119@gmail.com>
2022-03-27 13:55:34 +0900

    fix: test

--- src/components/progresses/progress_image/__tests__/pregress_image/default.test.tsx
+++ src/components/progresses/progress_image/__tests__/pregress_image/default.test.tsx
@@ -20,7 +20,7 @@ describe('ProgressImage', () => {
 
     const renderTest = TestingLibrary.render(<ProgressImage alt="" src="" />);
     expect(
-      '<div><div css="You have tried to stringify object returned from `css` function. It isn\'t supposed to be used directly (e.g. as value of the `className` prop), but rather handed to emotion so it can handle it (e.g. as value of `css` prop)."><svg viewBox="0 0 64 64" css="You have tried to stringify object returned from `css` function. It isn\'t supposed to be used directly (e.g. as value of the `className` prop), but rather handed to emotion so it can handle it (e.g. as value of `css` prop)."><circle cx="50%" cy="50%" r="15.9154"></circle></svg><svg viewBox="0 0 64 64" css="You have tried to stringify object returned from `css` function. It isn\'t supposed to be used directly (e.g. as value of the `className` prop), but rather handed to emotion so it can handle it (e.g. as value of `css` prop)."><circle cx="50%" cy="50%" r="15.9154"></circle></svg></div></div>',
+      '<div><div css="You have tried to stringify object returned from `css` function. It isn\'t supposed to be used directly (e.g. as value of the `className` prop), but rather handed to emotion so it can handle it (e.g. as value of `css` prop)."><svg viewBox="0 0 64 64" css="You have tried to stringify object returned from `css` function. It isn\'t supposed to be used directly (e.g. as value of the `className` prop), but rather handed to emotion so it can handle it (e.g. as value of `css` prop)."><circle cx="50%" cy="50%" r="15.9154"></circle></svg></div></div>',
     ).toBe(renderTest.baseElement.innerHTML);
   });
 });
